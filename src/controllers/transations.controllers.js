2b402bfad13fcbd206e451ce1579311b7723212b
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-11-19 18:35:00 -0300

    salvando info das transações no BD

--- src/controllers/transations.controllers.js
+++ src/controllers/transations.controllers.js
@@ -1,18 +1,39 @@
-import { transationSchema } from "../index.js";
+
 import { transationsCollection } from "../database/db.js";
 
 export async function postTransations (req,res){
-    const {type , value, name} = req.body;
+    const { type, value, description}  = req.body;
 
     try {
-
     await transationsCollection.insertOne({
-        type,
+        description,
         value,
-        name,
+        type,
     })
-    res.sendStatus(200).send("transação concluida");
+    res.sendStatus(201).send("transação concluida");
+    console.log("postou corretamente")
 }catch (err){
-    res.sendStatus(501).send(err)
+    console.log(err);
+    res.sendStatus(501).send(err);
 }
+}
+
+export async function getTransations (req,res) {
+    const limit = Number(req.query.limit);
+    const {description} = req.body;
+    
+    try{
+        const transatios = await transationsCollection
+        .find({description})
+        .limit(limit)
+        .toArray();
+
+        if (transatios.length===0) {
+            return res.sendStatus(404).send("Não foi encontrada nenhuma transação");
+        }
+        res.send(transatios);
+    }catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 }
